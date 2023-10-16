59e0b92c8bd2b7b544817dcfd0ca4f256bae6cc2
nabahsheikh <nabahsheikh2001@gmail.com>
2023-10-16 10:50:40 +0500

    Edit function

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,6 +13,9 @@ export class DashboardComponent {
   expenseName:string =''
   amount:number = 0
   totalAmount:number=0
+  selectedExpense: Expense | null = null
+
+
 
   ngOnInit() {
     const storedExpenses = localStorage.getItem('expenses');
@@ -22,8 +25,14 @@ export class DashboardComponent {
     }
   }addExpense() {
     if (this.expenseName && this.amount > 0) {
+      if(this.selectedExpense){
+        this.selectedExpense.name = this.expenseName
+        this.selectedExpense.amount = this.amount
+        this.selectedExpense = null;
+      }
+      else{
       const newExpense: Expense = { name: this.expenseName, amount: this.amount };
-      this.expenses.push(newExpense);
+      this.expenses.push(newExpense);}
       this.calculateTotalAmount();
 
       localStorage.setItem('expenses', JSON.stringify(this.expenses));
@@ -37,9 +46,18 @@ export class DashboardComponent {
     this.totalAmount = this.expenses.reduce((total, expense) => total + expense.amount, 0);
   }
 
-  editExpense(){
+  
+editExpense(expense: Expense) {
+  this.selectedExpense = expense;
+  this.expenseName = expense.name;
+  this.amount = expense.amount;
+}
+cancelEdit() {
+  this.selectedExpense = null;
+  this.expenseName = '';
+  this.amount = 0;
+}
 
-  }
   deleteExpense(){
 
   }
